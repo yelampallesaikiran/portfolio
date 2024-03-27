c899d24acf6f5c5ad9cc22f8b1842e17da46aec7
saikiran <reddysaikiran621@gmail.com>
2024-03-27 13:54:21 +0530

    my first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Add JavaScript functionality here if needed
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Add code to handle form submission (e.g., sending data to a server)
+  });
+  
